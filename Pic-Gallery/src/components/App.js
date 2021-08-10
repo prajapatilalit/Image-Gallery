c9701b97ba49bb2d63a7c112590e055de92a4ef2
prajapatilalit <prajapatilalit150992@gmail.com>
2021-08-11 00:36:50 +0530

    update unplash api call to render images

--- Pic-Gallery/src/components/App.js
+++ Pic-Gallery/src/components/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends Component {
-  onFormSubmit(text) {
-    console.log(text);
-  }
+  state = {
+    images: [],
+  };
+
+  onFormSubmit = async (text) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: text },
+      headers: {
+        Authorization: "Client-ID dvNGhJ2CNPL4XjWgTpVQYag0FCidzbO5bZSs6Pe0_Z4",
+      },
+    });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: 10 }}>
         <SearchBar onSubmit={this.onFormSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
